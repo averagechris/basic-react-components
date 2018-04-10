278d00df3c57d15c6f44513f0ffa8ac345dbdd52
mistahchris <chris@thesogu.com>
2018-04-10 14:24:21 -0500

    fix module export issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import ArrowIcon from "./components/arrowIcon.js";
-import Button from "./components/button.js";
-import CloseIcon from "./components/closeIcon.js";
-import Form from "./components/form/form.js";
-import FormInput from "./components/form/formInput.js";
-import Code from "./components/inlineCode.js";
-import Modal from "./components/modal/modal.js";
-import Gallery, { GalleryViewer } from "./components/modal/gallery.js";
+export ArrowIcon from "./components/arrowIcon.js";
+export Button from "./components/button.js";
+export CloseIcon from "./components/closeIcon.js";
+export Form from "./components/form/form.js";
+export FormInput from "./components/form/formInput.js";
+export Code from "./components/inlineCode.js";
+export Modal from "./components/modal/modal.js";
+export Gallery, { GalleryViewer } from "./components/modal/gallery.js";
