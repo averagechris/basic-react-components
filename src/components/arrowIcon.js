b991ccff3a709db2ae6497cca3faa38f119a035f
mistahchris <mistahcummings@gmail.com>
2018-04-11 00:24:28 -0500

    generify ArrowIcon

--- src/components/arrowIcon.js
+++ src/components/arrowIcon.js
@@ -3,23 +3,57 @@ import PropTypes from "prop-types";
 
 import { propValidator } from "../helpers.js";
 
-const POINTS_MAP = {
-  right: [
-    { x1: "1", y1: "1", x2: "25", y2: "20" },
-    { x1: "1", y1: "40", x2: "25", y2: "20" }
-  ],
-  left: [
-    { x1: "1", y1: "20", x2: "25", y2: "40" },
-    { x1: "1", y1: "20", x2: "25", y2: "1" }
-  ],
-  up: [
-    { x1: "20", y1: "1", x2: "1", y2: "25" },
-    { x1: "20", y1: "1", x2: "40", y2: "25" }
-  ],
-  down: [
-    { x1: "20", y1: "25", x2: "1", y2: "1" },
-    { x1: "20", y1: "25", x2: "40", y2: "1" }
-  ]
+const getPoints = (max, min, direction) => {
+  let points_map = {
+    right: [
+      {
+        x1: min,
+        y1: min,
+        x2: Math.floor(max * 0.5),
+        y2: Math.floor(max * 0.5)
+      },
+      { x1: min, y1: max, x2: Math.floor(max * 0.5), y2: Math.floor(max * 0.5) }
+    ],
+    left: [
+      {
+        x1: min,
+        y1: Math.floor(max * 0.5),
+        x2: Math.floor(max * 0.5),
+        y2: max
+      },
+      { x1: min, y1: Math.floor(max * 0.5), x2: Math.floor(max * 0.5), y2: min }
+    ],
+    up: [
+      {
+        x1: Math.floor(max * 0.5),
+        y1: min,
+        x2: min,
+        y2: Math.floor(max * 0.63)
+      },
+      {
+        x1: Math.floor(max * 0.5),
+        y1: min,
+        x2: max,
+        y2: Math.floor(max * 0.63)
+      }
+    ],
+    down: [
+      {
+        x1: Math.floor(max * 0.5),
+        y1: Math.floor(max * 0.63),
+        x2: min,
+        y2: min
+      },
+      {
+        x1: Math.floor(max * 0.5),
+        y1: Math.floor(max * 0.63),
+        x2: max,
+        y2: min
+      }
+    ]
+  };
+
+  return points_map[direction];
 };
 
 const ArrowIcon = ({
@@ -32,22 +66,25 @@ const ArrowIcon = ({
   strokeOpacity,
   onClick
 }) => {
+  let maxSize = height > width ? height : width;
+  let points = getPoints(maxSize, 1, direction);
+
   return (
     <div className="pointer w2 ma2 dim" onClick={onClick}>
       <svg
-        height={height}
-        width={width}
+        height={`${height}px`}
+        width={`${width}px`}
         version="1.1"
         xmlns="http://www.w3.org/svg"
       >
         <line
-          {...POINTS_MAP[direction][0]}
+          {...points[0]}
           stroke={color}
           strokeOpacity={strokeOpacity}
           strokeWidth={strokeWidth}
         />
         <line
-          {...POINTS_MAP[direction][1]}
+          {...points[1]}
           stroke={color}
           strokeOpacity={strokeOpacity}
           strokeWidth={strokeWidth}
@@ -61,8 +98,8 @@ ArrowIcon.propTypes = {
   color: PropTypes.oneOf(["white", "black"]),
   disabled: PropTypes.bool,
   direction: PropTypes.oneOf(["right", "left", "up", "down"]).isRequired,
-  height: propValidator.endsWithpx,
-  width: propValidator.endsWithpx,
+  height: PropTypes.number,
+  width: PropTypes.number,
   strokeWidth: propValidator.endsWithpx,
   strokeOpacity: propValidator.isDecimal,
   onClick: PropTypes.func.isRequired
@@ -71,8 +108,8 @@ ArrowIcon.propTypes = {
 ArrowIcon.defaultProps = {
   color: "white",
   disabled: false,
-  height: "40px",
-  width: "40px",
+  height: 40,
+  width: 40,
   strokeOpacity: "0.8",
   strokeWidth: "2px"
 };
