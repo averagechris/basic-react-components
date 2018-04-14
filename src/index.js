c2b3fd60fa4f2830fa353bf3ea3f0f8a5a64a4a7
mistahchris <mistahcummings@gmail.com>
2018-04-14 00:14:33 -0500

    add icons dir, move icon type componets there

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export ArrowIcon from "./components/arrowIcon.js";
+export ArrowIcon from "./components/icons/arrowIcon.js";
 export Button from "./components/button.js";
-export CloseIcon from "./components/closeIcon.js";
+export CloseIcon from "./components/icons/closeIcon.js";
 export Form from "./components/form/form.js";
 export FormInput from "./components/form/formInput.js";
 export Code from "./components/inlineCode.js";
 export Modal from "./components/modal/modal.js";
 export Gallery, { GalleryViewer } from "./components/modal/gallery.js";
 export TagItem from "./components/tagItem.js";
-export Spinner from "./components/spinner.js";
+export Spinner from "./components/icons/spinner.js";
