e79132dabe073d47aa65d4b586213bca2aa3a96b
mistahchris <mistahcummings@gmail.com>
2018-04-16 00:47:18 -0500

    add unfinished HamburgerIcon

---
+++ src/components/icons/hamburgerIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const HamburgerIcon = props => {
+  return <div>hello</div>;
+};
+
+HamburgerIcon.defaultProps = {};
+HamburgerIcon.propTypes = {};
+
+export default HamburgerIcon;
---
+++ src/components/icons/hamburgerIcon.test.js
@@ -0,0 +1,7 @@
+import PropTypes from "prop-types";
+
+import { runAllComponentSmokeTests } from "../componentTestHelpers.js";
+import HamburgerIcon from "./hamburgerIcon.js";
+
+const requiredProps = {};
+runAllComponentSmokeTests(HamburgerIcon, {});
