735f8de010fdf4855e57cc3ad2ea9d63d8fbe2d9
mistahchris <mistahcummings@gmail.com>
2018-04-17 23:01:29 -0500

    export new components

--- src/index.js
+++ src/index.js
@@ -10,10 +10,18 @@ export { propValidator, cssTypes, concatClassNames } from "./helpers.js";
 export css from "./thecss.css";
 
 // component exports
+
+// misc
 export Button from "./components/button.js";
+export Code from "./components/inlineCode.js";
+export TagItem from "./components/tagItem.js";
+
+// form
 export Form from "./components/form/form.js";
 export FormInput from "./components/form/formInput.js";
 export Select from "./components/form/select.js";
+
+// icons
 export ArrowIcon from "./components/icons/arrowIcon.js";
 export CloseIcon from "./components/icons/closeIcon.js";
 export ErrorIcon from "./components/icons/errorIcon.js";
@@ -21,7 +29,11 @@ export InfoIcon from "./components/icons/infoIcon.js";
 export OkayIcon from "./components/icons/okayIcon.js";
 export Spinner from "./components/icons/spinner.js";
 export xIcon from "./components/icons/xIcon.js";
-export Code from "./components/inlineCode.js";
+
+// modal
 export Gallery, { GalleryViewer } from "./components/modal/gallery.js";
 export Modal from "./components/modal/modal.js";
-export TagItem from "./components/tagItem.js";
+
+// banners
+export MessageBanner from "./components/banners/messageBanner.js";
+export Notification from "./components/banners/notification.js";
